4dd4a1ada5624b70ba653ac11d6526a5a3e86c24
Alex6dev <alexandresix2@gmail.com>
2023-06-12 11:47:30 +0200

    aside left and logout OK

--- src/app/left-side-bar/box-user-current/box-user-current.component.ts
+++ src/app/left-side-bar/box-user-current/box-user-current.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from 'src/app/interface/user';
+import { UserService } from '../../service/user.service';
 
 @Component({
-  selector: 'app-box-user-current',
+  selector: 'app-box-user-current [userCurrent]',
   templateUrl: './box-user-current.component.html',
   styleUrls: ['./box-user-current.component.css']
 })
 export class BoxUserCurrentComponent {
+  @Input() userCurrent! :User;
+  displayBox=false;
 
+  constructor(
+    private userService:UserService
+  ){}
+  setDisplayBox(){
+    this.displayBox=!this.displayBox;
+  }
+
+  logOut(){
+    this.userService.setUserInSession(undefined);
+    this.userService.setCurrentUser(undefined);
+  }
+  
 }
