c9cc9a7537767985660eb1f0436995a869263c13
Alex6dev <alexandresix2@gmail.com>
2023-06-19 11:24:04 +0200

    view user OK and fix send tweet of clean form in writeTweet

--- src/app/write-tweet/write-tweet.component.ts
+++ src/app/write-tweet/write-tweet.component.ts
@@ -36,6 +36,11 @@ export class WriteTweetComponent {
         this.tweetService.sendTweetInAPI(this.form);
       }
     }
+    this.imageInTweet=null;
+    this.form=new FormGroup({
+      content:new FormControl(null,[Validators.required,Validators.minLength(2),Validators.maxLength(256)]),
+      author:new FormControl(this.userCurrent.id,[Validators.required])
+    })
   }
   handleFiles(event:any) {
     const selectedfile = event.target.files;
